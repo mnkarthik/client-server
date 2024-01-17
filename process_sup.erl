958b7d89d115e9050300a7a8ec26894897fbaaa6
KARTHIK <apple@APPLEs-MacBook-Pro.local>
2024-01-17 12:38:42 +0530

    Version 1.1 - Add supervisor

---
+++ process_sup.erl
@@ -0,0 +1,32 @@
+-module(process_sup).
+
+-behaviour(supervisor).
+
+-export([init/1, start/0]).
+
+% start
+start() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+sup_flags() -> {one_for_one, 5, 10}.
+child_spec() -> [client_process(), server_process()].
+
+client_process() ->
+    {client_process
+    ,{client_process, start_link, []}
+    ,permanent
+    ,5000
+    ,worker
+    ,[client_process]}.
+
+server_process() ->
+    {server_process
+    ,{server_process, start_link, []}
+    ,permanent
+    ,5000
+    ,worker
+    ,[server_process]}.
+
+% init
+init([]) ->
+    {ok, {sup_flags(), child_spec()}}.
